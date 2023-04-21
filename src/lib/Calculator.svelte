86b089f7f420d5c5326ce83078c1c0f5c127baf7
iamqwertyloal <iamqwertyloal@gmail.com>
2023-04-21 14:07:42 +0500

    Finish markup and styling

--- src/lib/Calculator.svelte
+++ src/lib/Calculator.svelte
@@ -7,22 +7,22 @@
     <button class="clear">AC</button>
     <button class="swap">+/-</button>
     <button value="%">%</button>
-    <button class="backspace">&#9003;</button>
+    <button class="backspace orange">&#9003;</button>
     <button value="7">7</button>
     <button value="8">8</button>
     <button value="9">9</button>
-    <button value="/">÷</button>
+    <button class="orange" value="/">÷</button>
     <button value="4">4</button>
     <button value="5">5</button>
     <button value="6">6</button>
-    <button value="*">×</button>
+    <button class="orange" value="*">×</button>
     <button value="1">1</button>
     <button value="2">2</button>
     <button value="3">3</button>
-    <button value="-">-</button>
+    <button class="orange" value="-">-</button>
     <button value=".">,</button>
     <button value="0">0</button>
     <button value="=">=</button>
-    <button value="+">+</button>
+    <button class="orange" value="+">+</button>
   </div>
 </div>
