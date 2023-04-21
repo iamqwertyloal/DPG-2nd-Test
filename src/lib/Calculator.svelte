ba736e6c78d951c4e6d19b85804443c895bbc786
iamqwertyloal <iamqwertyloal@gmail.com>
2023-04-21 13:09:36 +0500

    Add starting markup and styles

---
+++ src/lib/Calculator.svelte
@@ -0,0 +1,28 @@
+
+<div class="calculator">
+  <div class="calculator-display">
+    <h1>42</h1>
+  </div>
+  <div class="calculator-buttons">
+    <button class="clear">AC</button>
+    <button class="swap">+/-</button>
+    <button value="%">%</button>
+    <button class="backspace">&#9003;</button>
+    <button value="7">7</button>
+    <button value="8">8</button>
+    <button value="9">9</button>
+    <button value="/">÷</button>
+    <button value="4">4</button>
+    <button value="5">5</button>
+    <button value="6">6</button>
+    <button value="*">×</button>
+    <button value="1">1</button>
+    <button value="2">2</button>
+    <button value="3">3</button>
+    <button value="-">-</button>
+    <button value=".">,</button>
+    <button value="0">0</button>
+    <button value="=">=</button>
+    <button value="+">+</button>
+  </div>
+</div>
